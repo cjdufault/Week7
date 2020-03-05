3de6d8236bd5a2c27a12250ae518c195524bc089
Connor Dufault <mx8434an@go.minnstate.edu>
2020-03-05 13:58:46 -0600

    built geo quiz 2

--- GeoQuizTwo/Form1.Designer.cs
+++ GeoQuizTwo/Form1.Designer.cs
@@ -28,20 +28,61 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.lblQuestion = new System.Windows.Forms.Label();
+            this.txtAnswer = new System.Windows.Forms.TextBox();
+            this.btnCheckAnswer = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // lblQuestion
+            // 
+            this.lblQuestion.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblQuestion.Location = new System.Drawing.Point(29, 30);
+            this.lblQuestion.Name = "lblQuestion";
+            this.lblQuestion.Size = new System.Drawing.Size(295, 69);
+            this.lblQuestion.TabIndex = 0;
+            this.lblQuestion.Text = "Question goes here";
+            this.lblQuestion.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // txtAnswer
+            // 
+            this.txtAnswer.Location = new System.Drawing.Point(29, 105);
+            this.txtAnswer.MaxLength = 20;
+            this.txtAnswer.Name = "txtAnswer";
+            this.txtAnswer.Size = new System.Drawing.Size(181, 20);
+            this.txtAnswer.TabIndex = 1;
+            // 
+            // btnCheckAnswer
+            // 
+            this.btnCheckAnswer.Location = new System.Drawing.Point(239, 102);
+            this.btnCheckAnswer.Name = "btnCheckAnswer";
+            this.btnCheckAnswer.Size = new System.Drawing.Size(85, 23);
+            this.btnCheckAnswer.TabIndex = 2;
+            this.btnCheckAnswer.Text = "Check Answer";
+            this.btnCheckAnswer.UseVisualStyleBackColor = true;
+            this.btnCheckAnswer.Click += new System.EventHandler(this.btnCheckAnswer_Click);
             // 
             // Form1
             // 
+            this.AcceptButton = this.btnCheckAnswer;
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(350, 153);
+            this.Controls.Add(this.btnCheckAnswer);
+            this.Controls.Add(this.txtAnswer);
+            this.Controls.Add(this.lblQuestion);
             this.Name = "Form1";
             this.Text = "Geography Quiz 2 -- Electric Boogaloo";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Label lblQuestion;
+        private System.Windows.Forms.TextBox txtAnswer;
+        private System.Windows.Forms.Button btnCheckAnswer;
     }
 }
 
--- GeoQuizTwo/Form1.cs
+++ GeoQuizTwo/Form1.cs
@@ -12,9 +12,95 @@ namespace GeoQuizTwo
 {
     public partial class Form1 : Form
     {
+        readonly SortedList<string, string> questions = new SortedList<string, string>() // { question, answer }
+        {
+            { "What is the state capital of California?", "Sacramento" },
+            { "What is the tallest mountain on Earth?",  "Everest" },
+            { "What country extends the furthest south?", "Chile" }
+        };
+        List<string> answers = new List<string>();
+
+        DateTime startTime; // the time the quiz starts
+        int questionCounter = 0; // keep track of what question we're on
+        int score = 0;
+
         public Form1()
         {
             InitializeComponent();
+        }  
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            startTime = DateTime.Now; // record the start time
+            ShowQuestion();
+        }
+
+        private void btnCheckAnswer_Click(object sender, EventArgs e)
+        {
+            // check answer and add to list
+            string answer = txtAnswer.Text.ToLower();
+            CheckAnswer(answer);
+            answers.Add(answer);
+
+            // increment counter and show the next question if there are any left, game over if not
+            questionCounter++;
+            if (questionCounter < questions.Count)
+            {
+                txtAnswer.Clear();
+                ShowQuestion();
+            }
+            else
+            {
+                GameOver();
+            }
+        }
+
+        private void CheckAnswer(string answer)
+        {
+            string question = questions.Keys[questionCounter];
+            string correctAnswer = questions[question];
+
+            // if correct answer is in inputed answer, increment the score
+            if (answer.Contains(correctAnswer.ToLower()))
+            {
+                score++;
+                MessageBox.Show($"That's correct!\n\nScore = {score}", "Result");
+            }
+            else
+            {
+                MessageBox.Show($"That's wrong! The correct answer is {correctAnswer}.\n\nScore = {score}", "Result");
+
+            }
+        }
+
+        // displays the question corresponding to the current state of the questionCounter
+        private void ShowQuestion()
+        {
+            string nextQuestion = questions.Keys[questionCounter];
+            lblQuestion.Text = $"Question {questionCounter + 1}:\n{nextQuestion}";
+        }
+
+        private void GameOver()
+        {
+            DateTime endTime = DateTime.Now;
+            TimeSpan timeElapsed = endTime.Subtract(startTime);
+
+            StringBuilder builder = new StringBuilder();
+
+            // show all questions, the correct answers, and the player's answers
+            for (int i = 0; i < questions.Count; i++)
+            {
+                string question = questions.Keys[i];
+                builder.Append($"{question}\n\tCorrect Answer: {questions[question]}\n\tYour answer: {answers[i]}\n");
+            }
+
+            // show final stats
+            builder.Append($"\nTime elapsed: {timeElapsed.ToString(@"hh\:mm\:ss")}\n");
+            builder.Append($"Final Score: {score}");
+
+            MessageBox.Show(builder.ToString(), "Final Results");
+
+            this.Dispose(); // quit
         }
     }
 }
